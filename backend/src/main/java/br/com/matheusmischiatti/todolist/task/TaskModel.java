22051c9818b76f52474db73017e9528d622b49ee
Matheus Bosa Mischiatti <matheusbosa95@gmail.com>
2023-10-14 14:52:54 -0300

    terceira aula

--- backend/src/main/java/br/com/matheusmischiatti/todolist/task/TaskModel.java
+++ backend/src/main/java/br/com/matheusmischiatti/todolist/task/TaskModel.java
@@ -1,11 +1,10 @@
-package br.com.matheusmischiatti.todolist.user;
+package br.com.matheusmischiatti.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,18 +12,24 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name = "tb_users")
-public class UserModel {
-	
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
 	@Id
 	@GeneratedValue(generator = "UUID")
 	private UUID id;
+
+	private String description;
 	
-	@Column(unique = true)
-	private String username;
-	private String name;
-	private String password;
+	@Column(length = 50)
+	private String title;
 	
+	private LocalDateTime startAt;
+	private LocalDateTime endAt;
+	private String priority;
+
+	private UUID idUser;
+
 	@CreationTimestamp
 	private LocalDateTime createdAt;
 
